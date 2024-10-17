f96c1803f80a33a1ba0f0403ce4fadd66fe08bb4
Sibgatullah <sibgatullah881@gmail.com>
2024-10-17 19:22:49 +0600

    fixed bugs

--- login_post.php
+++ login_post.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
+require 'db.php';
 
 
-$email = $_SESSION['email'];
-$password = $_SESSION['password'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 $flag = false;
 
@@ -12,10 +13,14 @@ if(empty($email)){
     $_SESSION['email_err'] = 'Enter Email';
 }
 else{
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $flag= true;
-        $_SESSION['email_err'] = 'Invalid Email';
-    }
+    // if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    //     $flag= true;
+    //     $_SESSION['email_err'] = 'Invalid Email';
+    // }
+    $select = "SELECT COUNT(*)as totla FROM users WHERE email='$email'";
+    $select_querry= mysqli_query($db_connect, $select);
+    $select_result = mysqli_fetch_assoc($select_result);
+    echo $select_result['total'];
 }
 
 if(empty($password)){
@@ -27,6 +32,7 @@ if(empty($password)){
 if($flag){
     header('location:login.php');
 }
+
 
 
 ?>
