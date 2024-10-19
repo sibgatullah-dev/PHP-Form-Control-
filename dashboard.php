bf9d2628fda8c17954280634f7b7491e4c155ec9
Sibgatullah <sibgatullah881@gmail.com>
2024-10-19 23:30:37 +0600

    added logout funcion

--- dashboard.php
+++ dashboard.php
@@ -9,3 +9,10 @@ if(!isset($_SESSION['login_confirm'])){
 
 
 <h1>Welcome to Dashboard!!</h1>
+
+
+<div class="mt-4">
+    <a href="logout.php">
+        <button type="submit" class="btn btn-primary">Logout</button>
+    </a>
+</div>
